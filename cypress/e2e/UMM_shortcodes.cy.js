9f4b01da229c2fc64dcf2f0b12505d54e9abb54c
riham.kamal <riham.kamal@flairstech.com>
2023-12-23 02:02:33 +0200

    update

--- cypress/e2e/UMM_shortcodes.cy.js
+++ cypress/e2e/UMM_shortcodes.cy.js
@@ -1,34 +1,45 @@
 describe('UMM ShortCodes Scenarios using Mobile Commons Platform', () => {
     const env = Cypress.env();
-  
 
- 
+
+
     context('Create shortcodes', () => {
         it('Get short code reports for super user', () => {
-      
+
 
             cy.GetShortCodesReports("GET") // Corrected method name to match the command
-          
+
                 .then((response) => {
-                  
+
                     expect(response.status).to.eq(200);
                     expect(response.shortcode_number).to.eq("24474"); // Adjust property based on the actual response structure
                     expect(response.mos).to.eq("0"); // Adjust property based on the actual response structure
                     expect(response.mts).to.eq("89"); // Adjust property based on the actual response structure
                 });
         });
-        it.only('Attach short codes to acompany', () => {
-         
+        it('Attach short codes to acompany', () => {
             cy.PostsShortcCodeToCompany("POST")
-                  .then((response) => {
-                      expect(response.status).to.eq(200);
-                      expect(response.companies.is_parent).to.eq(true)
-                      expect(response.companies.name).to.eq("Test Parent Originator")
-                      expect(response.companies.short_name).to.eq("Parent Originator")
-                      expect(response.companies.status).to.eq("Active")
-                      
-  
-                  });
-          });
+                .then((response) => {
+                    expect(response.status).to.eq(200);
+                    expect(response.companies.is_parent).to.eq(true)
+                    expect(response.companies.name).to.eq("Test Parent Originator")
+                    expect(response.companies.short_name).to.eq("Parent Originator")
+                    expect(response.companies.status).to.eq("Active")
+
+
+                });
+        });
+        it.only('Detach company from shortcodes', () => {
+            cy.DetachCompanyFromShortCodes("delete")
+                .then((response) => {
+                    expect(response.status).to.eq(200);
+                    expect(response.active).to.eq(true);
+                    expect(response.dedectied).to.eq(false)
+                    expect(response.shortcode_number).to.eq("24474")
+
+                });
+
+
+        });
     });
 });
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,18 +1,18 @@
 
 const env = Cypress.env();
-Cypress.Commands.add('GetShortCodesReports', ( method,
+Cypress.Commands.add('GetShortCodesReports', (method,
   { header = { Cookie: env.cookie },
     params = { shortcode_id: env.shortcode_id }
   } = {}) => {
-    const domain = env.domain; // Fix typo in customconfig
-    const shortCodeUrl = env.shortcodes; // Fix typo in customconfig
-      const url = `${domain}/${shortCodeUrl}/${env.shortcode_id}/reports`; // Replace with your actual URL
-  
+  const domain = env.domain; // Fix typo in customconfig
+  const shortCodeUrl = env.shortcodes; // Fix typo in customconfig
+  const url = `${domain}/${shortCodeUrl}/${env.shortcode_id}/reports`; // Replace with your actual URL
+
   cy.request({
     method: method,
-    url:  url , // aFix string concatenation
+    url: url, // aFix string concatenation
     headers: header,
-   // qs: params, // Using qs for query parameters
+    // qs: params, // Using qs for query parameters
   }).then((response) => response.headers['set-cookie']);
 });
 
@@ -29,5 +29,20 @@ Cypress.Commands.add("PostsShortcCodeToCompany", (method,
     url: url,
     headers: headers,
     body: body
+  }).then((response) => response.headers["set-cookie"]);
+});
+Cypress.Commands.add("DetachCompanyFromShortCodes", (method,
+  { headers = { cookie: env.cookie, },
+    params = { id: env.company_id, shortcode_id: env.shortcode_id }
+  } = {}) => {
+  const domain = env.domain; // Fix typo in customconfig
+  const shortCodeUrl = env.shortcodes; // Fix typo in customconfig
+  const url = `${domain}/${shortCodeUrl}/${env.shortcode_id}/companies/${env.company_id}`; // Replace with your actual URL
+  cy.request({
+    method: method,
+    url: url,
+    headers: headers,
+
+
   }).then((response) => response.headers["set-cookie"]);
 });
